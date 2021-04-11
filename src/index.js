620d9c32e5db2d42c0e8637dadfe1a1b72b2f05d
KANGJUHYEON <alinos@naver.com>
2021-04-11 19:00:08 +0900

    Actions

--- src/index.js
+++ src/index.js
@@ -4,10 +4,27 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-const reducer = (state = 0) => {
-  // console.log(state);
-  return state;
+const reducer = (count = 0, action) => {
+  console.log(count, action);
+  if (action.type === "ADD"){
+    return count + 1;
+    // console.log('they are telling me to add one');
+  } else if(action.type === "MINUS"){
+    return count - 1;
+  } else{
+    return count;
+  }
 };
 
 const store = createStore(reducer);
+
+
+store.dispatch({type: "ADD"});
+store.dispatch({type: "ADD"});
+store.dispatch({type: "ADD"});
+store.dispatch({type: "ADD"});
+store.dispatch({type: "ADD"});
+store.dispatch({type: "MINUS"}); 
+// 결과값 4
+
 console.log(store.getState());
