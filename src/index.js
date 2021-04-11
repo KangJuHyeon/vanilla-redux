5addf868a16ba34b258c70fc44dd043b49be4c9f
KANGJUHYEON <alinos@naver.com>
2021-04-11 18:47:49 +0900

    Store and Reducer

--- src/index.js
+++ src/index.js
@@ -1,25 +1,13 @@
+import { createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
-}
-
-const handleAdd = () =>{
-  // console.log("add")
-  count = count + 1;
-  updateText();
-}
-
-const handleMinus = () => {
-  count = count - 1;
-  updateText();
+const reducer = (state = 0) => {
+  // console.log(state);
+  return state;
 };
 
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+const store = createStore(reducer);
+console.log(store.getState());
