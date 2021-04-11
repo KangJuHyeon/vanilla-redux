c8f888b40d41142ac89785bd895a240d9a3984cd
KANGJUHYEON <alinos@naver.com>
2021-04-11 19:26:45 +0900

    Recap Refactor and finished counter

--- src/index.js
+++ src/index.js
@@ -6,16 +6,25 @@ const number = document.querySelector("span");
 
 number.innerText = 0;
 
+const ADD = "ADD";
+const MINUS = "MINUS"
+
 const reducer = (count = 0, action) => {
-  if (action.type === "ADD"){
-    return count + 1;
-  } else if(action.type === "MINUS"){
-    return count - 1;
-  } else{
-    return count;
+  switch(action.type){
+    case ADD:
+      return count + 1
+    case MINUS:
+      return count - 1
+    default:
+      return count;
   }
 };
-
+// if, else if 문을 사용할수도 있지만 대부분 사람들은 대신 switch를 사용한다
+// 왜? 이것이 첫 번째 improvement(개선 사항)이다.
+// 이것은 redux 공식 홈페이지에서도 있다.
+// 두 번째 improvement는 우리가 string을 사용한다는 것이다. => string을 쓰는 대신에 string을 담아주는 함수를 사용하는 것이 더 좋다
+// 왜? 위에 처럼 string을 사용한다면 자바스크립트가 아무것도 말해주지 않는다. 하지만 string을 함수에 담아서 사용하면 어디가 잘못되었다고 알려준다.
+// 어디 부분을 잘못하나요? action 디스패치 안에 ADD가 ADDD 이렇게 잘못되면 자바스크립트가 알려준다.
 
 const store = createStore(reducer);
 
@@ -23,7 +32,7 @@ const onChange = () => {
   number.innerText = store.getState();
 }
 
-store.subscribe(onChange);  // 이 function은 store에 변화가 있을 때마다 감지해서 불려질 거야!
+store.subscribe(onChange); 
 
 const handleAdd = () => {
   store.dispatch({type: "ADD" });
